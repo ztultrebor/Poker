85b755fb4e68f33a0013da0671c6fd066b321917
Robert Lutz <robertlutzi@gmail.com>
2024-01-20 01:45:36 -0500

    add hand-rank function

--- poker.rkt
+++ poker.rkt
@@ -17,8 +17,8 @@
 
 
 (define (deal n_players n_cards)
-    ; N N -> [ListOf [ListOf String]]
-    ; Creates a series of hands dealt from a single deck
+    """ N N -> [ListOf [ListOf String]]
+    Creates a series of hands dealt from a single deck"""
     (local ( 
         (define shuffled (shuffle deck))
         (define (deal-a-round cards p)
@@ -56,11 +56,44 @@
             Counts up the number of a given rank in a hand"""
             (cond
                 [(empty? this-rank) #f]
-                [else (if (= (count (lambda (r) (= (first this-rank) r)) ranks) n)
+                [else (if (= n (count (lambda (r) (= (first this-rank) r)) ranks))
                             (first this-rank)
                             (count-ranks (rest this-rank)))])))
         ; - IN -
         (count-ranks ranks)))
+
+
+(define (hand-rank hand)
+    """ [ListOf String -> [Tuple N]
+    determines the relative value of a poker hand. 
+    Only winning matters, so not a quantitative measure"""
+    (local (
+            (define suits (map (lambda (c) (second (string->list c))) hand))
+            (define ranks (sort (map (lambda (c) (card-rank c)) hand) >))
+            (define pair (kind 2 ranks))
+            (define pair2 (kind 2 (sort ranks <)))
+            (define three-kind (kind 3 ranks))
+            (define four-kind (kind 4 ranks))
+            (define straight (andmap (lambda (r1 r2) (= (- r1 r2) 1))
+                                    (reverse (rest (reverse ranks))) (rest ranks)))
+            (define flush (= (length (set->list (list->set suits))) 1)))
+            ; - IN -
+            (cond
+                [(and flush straight) (cons 8 ranks)]
+                [(true? four-kind) (cons 7 (cons four-kind ranks))]
+                [(and (true? three-kind) (true? pair)) (cons 6 (cons three-kind (cons pair ranks)))]
+                [flush (cons 5 ranks)]
+                [straight (cons 4 ranks)]
+                [(true? three-kind) (cons 3 (cons three-kind ranks))]
+                [(and (true? pair) (true? pair2)) (cons 2 (cons pair (cons pair2 ranks)))]
+                [(true? pair) (cons 1 (cons pair ranks))]
+                [else (cons 0 ranks)])))
+
+
+(define (true? x)
+    """ [Maybe X] -> Boolean
+    Like false?, but the opposite"""
+    (not (false? x)))
 
 
 
@@ -74,7 +107,13 @@
 (check-equal? (kind 2 (list 5 14 5 13 5)) #f)
 (check-equal? (kind 3 (list 5 14 5 13 4)) #f)
 (check-equal? (kind 2 (list 5 14 5 14 5)) 14)
-
+(check-equal? (hand-rank '("KD" "9S" "TD" "TC" "9H")) '(2 10 9 13 10 10 9 9))
+(check-equal? (hand-rank '("9D" "9S" "TD" "TC" "9H")) '(6 9 10 10 10 9 9 9))
+(check-equal? (hand-rank '("9D" "8S" "7D" "6C" "5H")) '(4 9 8 7 6 5))
+(check-equal? (hand-rank '("KD" "9D" "2D" "TD" "9D")) '(5 13 10 9 9 2))
+(check-equal? (hand-rank '("KD" "9S" "TD" "9C" "9H")) '(3 9 13 10 9 9 9))
+(check-equal? (hand-rank '("9D" "9S" "TD" "9C" "9H")) '(7 9 10 9 9 9 9))
+(check-equal? (hand-rank '("9D" "8D" "7D" "6D" "5D")) '(8 9 8 7 6 5))
 
 ;======================
 ; action!
